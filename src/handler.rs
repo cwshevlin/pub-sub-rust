77182c3e11a26036d0c318fa666fd123c857904b
Colin Shevlin <colin@classkick.com>
2022-02-03 12:19:53 -0600

    Just one last type error.

--- src/handler.rs
+++ src/handler.rs
@@ -6,19 +6,17 @@ use warp::{Rejection, hyper::StatusCode};
 use crate::Reply;
 use crate::ws;
 
-// TODO CWS: can all of these be a impl of Reply??
-
-pub async fn register_handler(body: RegisterRequest, clients: Clients) -> Result<Value, Rejection> {
+pub async fn register_handler(body: RegisterRequest, clients: Clients) -> Result<impl Reply, Rejection> {
     let user_id = body.user_id;
-    let uuid = Uuid::new_v4().simple().to_string();
+    let uuid = Uuid::new_v4().to_string();
   
     register_client(uuid.clone(), user_id, clients).await;
     Ok(json!({
       "url": format!("ws://127.0.0.1:8000/ws/{}", uuid),
-    }))
+    }).to_string())
   }
     
-async fn register_client(id: String, user_id: usize, clients: Clients) -> Result<Value, Rejection> {
+async fn register_client(id: String, user_id: usize, clients: Clients) -> Result<impl Reply, Rejection> {
     clients.lock().await.insert(
         id,
         Client {
@@ -27,29 +25,27 @@ async fn register_client(id: String, user_id: usize, clients: Clients) -> Result
             sender: None,
         },
     );
-    Ok(json!({}))
+    Ok(StatusCode::OK)
 }
 
-pub async fn unregister_handler(id: String, clients: Clients) -> Result<Value, Rejection> {
+pub async fn unregister_handler(id: String, clients: Clients) -> Result<impl Reply, Rejection> {
     clients.lock().await.remove(&id);
-    Ok(json!({}))
+    Ok(StatusCode::OK)
 }
 
 pub async fn ws_handler(ws: warp::ws::Ws, id: String, clients: Clients) -> Result<impl Reply, Rejection> {
     let client = clients.lock().await.get(&id).cloned();
     match client {
-      Some(c) => {
-          Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, id, clients, c)))
-      },
+      Some(c) => Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, id, clients, c))),
       None => Err(warp::reject::not_found()),
     }
 }
 
-pub async fn health_handler() -> Result<Value, Rejection> {
-    Ok(json!({}))
+pub async fn health_handler() -> Result<impl Reply, Rejection> {
+    Ok(StatusCode::OK)
 }
 
-pub async fn publish_handler(body: Event, clients: Clients) -> Result<Value, Rejection> {
+pub async fn publish_handler(body: Event, clients: Clients) -> Result<impl Reply, Rejection> {
     clients
         .lock()
         .await
@@ -65,5 +61,5 @@ pub async fn publish_handler(body: Event, clients: Clients) -> Result<Value, Rej
         }
     });
 
-    Ok(json!({}))
+    Ok(StatusCode::OK)
 }
