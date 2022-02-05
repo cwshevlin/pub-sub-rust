ad0be376bd9af07bdf1be7bd3cd82f8b24450850
Colin Shevlin <colin@classkick.com>
2022-02-05 15:17:55 -0600

    Adds locks for the mutex.

--- src/ws.rs
+++ src/ws.rs
@@ -1,19 +1,64 @@
-use warp::ws::WebSocket;
+use warp::ws::{Message, WebSocket};
 use crate::client::{Clients, Client};
 use tokio::sync::mpsc;
-use futures::StreamExt;
+use futures::{StreamExt};
 use tokio_stream::wrappers::UnboundedReceiverStream;
+use serde::{Deserialize};
+use serde_json::from_str;
 
+
+#[derive(Deserialize, Debug)]
+pub struct TopicsRequest {
+    topics: Vec<String>,
+}
 
 pub async fn client_connection(ws: WebSocket, id: String, clients: Clients, mut client: Client) {
     let (client_ws_tx, mut client_ws_rx) = ws.split();
-    let (client_tx, client_rx) = mpsc::unbounded_channel::<Result<_, _>>();
+    let (client_tx, client_rx) = mpsc::unbounded_channel::<Result<Message, warp::Error>>();
     let client_rx = UnboundedReceiverStream::new(client_rx); 
 
-    tokio::task::spawn(client_rx.forward(client_ws_tx).map(|result| {
-        if let Err(e) = result {
-            println!("error");
+    tokio::task::spawn(client_rx.forward(client_ws_tx));
+    client.sender = Some(client_tx);
+    clients.lock().await.insert(id.clone(), client);
+
+    println!("{} connected", id);
+
+    while let Some(result) = client_ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("error receiving ws message for id: {}): {}", id.clone(), e);
+                break;
+            }
+        };
+        client_msg(&id, msg, &clients).await;
+    }
+
+    clients.lock().await.remove(&id);
+    println!("{} disconnected", id);
+}
+
+async fn client_msg(id: &str, msg: Message, clients: &Clients) {
+    println!("received message from {}: {:?}", id, msg);
+    let message = match msg.to_str() {
+        Ok(v) => v,
+        Err(_) => return,
+    };
+
+    if message == "ping" || message == "ping\n" {
+        return;
+    }
+
+    let topics_req: TopicsRequest = match from_str(&message) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("error while parsing message to topics request: {}", e);
+            return;
         }
-        result
-    }));
+    };
+
+    let mut locked = clients.lock().await;
+    if let Some(v) = locked.get_mut(id) {
+        v.topics = topics_req.topics;
+    }
 }
